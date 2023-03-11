fbb157fb9760332f337e845b0e97693fe68769fc
PG22Arseniy <pg22arseniy@vfs.com>
2023-03-11 09:19:03 -0800

    login naviagate if logged out

--- client/src/pages/layouts/RootLayout.tsx
+++ client/src/pages/layouts/RootLayout.tsx
@@ -1,7 +1,12 @@
-import { Outlet } from "react-router-dom"
+import { Navigate, Outlet } from "react-router-dom"
+import { useAuth } from "../../context/AuthContext"
 
 export const RootLayout = () => {
+
+    const {user} = useAuth()
+
+    if (user == null) return <Navigate to="/login"/>
     return (
         <Outlet/>
     ) 
-}
+} 
