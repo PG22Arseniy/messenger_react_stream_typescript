7e9fc18c9cfbe2f7db22f9e2e600a256cd7732f7
PG22Arseniy <pg22arseniy@vfs.com>
2023-03-11 19:20:38 -0800

    all channels displayed, active channel styled with image and text

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,8 @@
 import { Channel, ChannelHeader, ChannelList, ChannelListMessengerProps, Chat, LoadingIndicator, MessageInput, MessageList, Window, useChatContext } from "stream-chat-react"
 import { useAuth, useLoggedInAuth } from "../context/AuthContext"
 import "../styles/Messenger.css" 
+import { CustomButton } from "../components/CustomButton"
+import { useNavigate } from "react-router-dom"
 
 export const Home = () => {
 
@@ -8,8 +10,8 @@ export const Home = () => {
 
     if (streamChat == null) return <LoadingIndicator/> 
 
-    return <Chat client={streamChat}>
-        <ChannelList sendChannelsToList filters={{members: {$in: [user.id]}}}/>
+    return <Chat client={streamChat}> 
+        <ChannelList List={Channels} sendChannelsToList filters={{members: {$in: [user.id]}}}/>
         <Channel>
             <Window>
                 <ChannelHeader/>
@@ -22,7 +24,50 @@ export const Home = () => {
 
 
 const Channels = ({loadedChannels}: ChannelListMessengerProps) => {
+
+    const navigate = useNavigate();
     const {setActiveChannel, channel: activeChannel} = useChatContext()
-    
-    return <div className="channelsSection"></div>
+   
+
+
+    return (
+    <div className="channelsSection">
+        <CustomButton onClick={()=> navigate("channel/new")}>New Conversation</CustomButton>
+        <hr/>
+
+        {
+         loadedChannels!= null && loadedChannels.length > 0 ?
+            loadedChannels.map (channel => {
+                const isActive = channel === activeChannel
+                const activityClass = isActive ? "activeChannel" : "nonActiveChannel"
+
+                return (
+                    <CustomButton 
+                        onClick={()=> setActiveChannel(channel)} 
+                        disabled = {isActive}   
+                        className={`channelButton ${activityClass}`}  
+                        key={channel.id}>
+                        
+                        {
+                            channel.data?.image && (
+                                <img 
+                                    src = {channel.data.image}
+                                    className="channelImg"
+                                    title="image" 
+                                />
+                            )
+                        }
+
+                        <div className="channelName">
+                            {channel.data?.name || channel.id}
+                        </div>
+
+                    </CustomButton>
+                )
+            })
+            : "No conversations"
+        }
+        
+    </div> 
+    )
 }
